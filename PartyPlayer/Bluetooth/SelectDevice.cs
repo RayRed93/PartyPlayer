22f0d7164280cd1601a69ca840deb146cb60b472
Pawel Brudnicki <pawel.brudnicki@klimat-solec.pl>
2015-03-14 14:39:54 +0100

    Paired devices on listview with icon(needs to be resize)

--- PartyPlayer/Bluetooth/SelectDevice.cs
+++ PartyPlayer/Bluetooth/SelectDevice.cs
@@ -26,9 +26,10 @@ namespace PartyPlayer.Bluetooth
 			View view = convertView;
 			if (view == null)
 				view =  (base.Context.GetSystemService(Activity.LayoutInflaterService) as LayoutInflater).Inflate(Resource.Layout.bluetooth_device, null);
+		    view.FindViewById<TextView>(Resource.Id.txtView).Text = base.GetItem(position);
 			if (position < SelectDevice.pariedDevicesCount)
 			{
-				view.FindViewById<Button>(Resource.Id.txtView).SetCompoundDrawables(base.Context.Resources.GetDrawable(Resource.Drawable.paired),
+                view.FindViewById<TextView>(Resource.Id.txtView).SetCompoundDrawablesWithIntrinsicBounds(base.Context.Resources.GetDrawable(Resource.Drawable.paired),
 					null, null, null);
 			}
 

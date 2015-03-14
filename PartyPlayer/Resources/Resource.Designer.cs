6522f61c1d84ea00e027da231a3527d99fa730f0
slawek9665 <sak126p@gmail.com>
2015-03-14 16:24:55 +0100

    commit od Sławka

--- PartyPlayer/Resources/Resource.Designer.cs
+++ PartyPlayer/Resources/Resource.Designer.cs
@@ -152,13 +152,16 @@ namespace PartyPlayer
 			public const int bluetooth_device = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int device_list = 2130903041;
+			public const int bluetooth_device_paired = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			public const int device_list = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Player = 2130903043;
+			public const int Main = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Player = 2130903044;
 			
 			static Layout()
 			{
